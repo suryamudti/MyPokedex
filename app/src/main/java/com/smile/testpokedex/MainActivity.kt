49fe371000ec5f65efcad4e6cf112132c90bcfa9
suryamudti.salmat <suryamudti.salmat@grabtaxi.com>
2025-06-18 02:11:27 +0800

    implement CI and update config

--- app/src/main/java/com/smile/testpokedex/MainActivity.kt
+++ app/src/main/java/com/smile/testpokedex/MainActivity.kt
@@ -3,24 +3,18 @@ package com.smile.testpokedex
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
 import androidx.compose.runtime.remember
-import androidx.compose.ui.graphics.BlendMode.Companion.Color
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.navigation.NavType
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
-import androidx.navigation.compose.navArgument
 import androidx.navigation.compose.rememberNavController
+import androidx.navigation.navArgument
 import com.smile.testpokedex.ui.pokemondetail.PokemonDetailScreen
 import com.smile.testpokedex.ui.pokemonlist.PokemonListScreen
 import com.smile.testpokedex.ui.theme.TestPokedexTheme
 import dagger.hilt.android.AndroidEntryPoint
-import java.util.*
+import java.util.Locale
 
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
--- app/src/main/java/com/smile/testpokedex/ui/pokemonlist/PokemonListViewModel.kt
+++ app/src/main/java/com/smile/testpokedex/ui/pokemonlist/PokemonListViewModel.kt
@@ -96,6 +96,8 @@ class PokemonListViewModel @Inject constructor(
                 is Resource.Error -> {
                     loadError.value = result.message!!
                 }
+
+                is Resource.Loading<*> -> {}
             }
         }
     }
